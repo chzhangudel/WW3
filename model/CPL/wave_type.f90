4884bdde7cdcfd8222d05ab16aa7dc228ebbe0d1
Brandon Reichl <Brandon.Reichl@noaa.gov>
2020-03-02 09:51:34 -0500

    Updating CPL files for new Stokes passing

--- model/CPL/wave_type.f90
+++ model/CPL/wave_type.f90
@@ -13,18 +13,27 @@ type wave_data_type
   logical                 :: pe
   logical                 :: Waves_Is_Init = .false.
   integer                 :: xtype
+  integer                 :: num_Stk_bands
   integer, pointer, dimension(:)   :: pelist   =>NULL() !< Used for flux-exchange.
   logical, pointer, dimension(:,:) :: ocean_pt =>NULL() !< An array that indicates ocean points as true.
 
+  real, pointer, dimension(:) :: stk_wavenumbers => NULL() !< Used for stokes drift
+
   ! These fields are used to provide information about the waves to the atmosphere/ocean/ice
-  real, pointer, dimension(:,:,:) :: &
+  real, pointer, dimension(:,:) :: &
        HS => NULL(), & !< The significant wave height [m]
        ustk0_mpp => NULL(), &
        vstk0_mpp => NULL(), &
        ustk0_glo => NULL(), &
        vstk0_glo => NULL()
 
-  integer, dimension(:,:,:), pointer :: & ! (lon, lat,tile)
+  real, pointer, dimension(:,:,:) :: &
+       ustkb_mpp => NULL(), &
+       vstkb_mpp => NULL(), &
+       ustkb_glo => NULL(), &
+       vstkb_glo => NULL()
+
+  integer, dimension(:,:), pointer :: & ! (lon, lat,tile)
        glob_loc_X => NULL(), & !
        glob_loc_Y => NULL()
 
@@ -49,16 +58,18 @@ type atmos_wave_boundary_type
 end type atmos_wave_boundary_type
 
 type ice_wave_boundary_type
-   real, dimension(:,:,:), pointer :: & ! (lon, lat,tile)
-        wavgrd_Ucurr_mpp => NULL(), & !
-        wavgrd_Vcurr_mpp => NULL()
-   real, dimension(:,:,:), pointer :: & ! (lon, lat,tile)
-        wavgrd_ucurr_glo => NULL(), & !
-        wavgrd_vcurr_glo => NULL()
 
    real, dimension(:,:,:), pointer :: & ! (lon, lat,tile)
+        wavgrd_Ucurr_mpp => NULL(), & !
+        wavgrd_Vcurr_mpp => NULL(), & !
+        wavgrd_ucurr_glo => NULL(), & !
+        wavgrd_vcurr_glo => NULL(), & !
         icegrd_ustk0_mpp => NULL(), & !
         icegrd_vstk0_mpp => NULL()
+
+   real, dimension(:,:,:,:), pointer :: & ! (lon, lat,tile,Nstk)
+        icegrd_ustkb_mpp => NULL(), & !
+        icegrd_vstkb_mpp => NULL()
 
    integer :: xtype             !REGRID, REDIST or DIRECT
 
